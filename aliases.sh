3afd34efd3df4afe6b04e7ea72dd35119d4ebd4a
Trisha Chittajallu <z5589661@ad.unsw.edu.au>
2025-08-31 23:56:51 +1000

    feat(aliases): add fun git aliases

---
+++ aliases.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# aliases.sh
+
+alias yeet='git add'
+alias yeetall='git add .'
+alias skrrt='git commit -m'
+alias receipts='git log'
+alias rewind='git restore --staged'
+alias oopsie='git commit --amend'
+alias cleanse='git reset --hard'
+alias sendit='git push'
+alias gimme='git pull'
+alias bigback='git add . && git commit -m "quick fix" && git push'
+alias newhuzz='git checkout -b'
+alias dump='git push && git branch -d'
+alias spill='git status'
+alias aura='git log --pretty=oneline --graph --decorate'
+alias packitup='git stash'
+alias cook='git diff'
