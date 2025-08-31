c051a6ee40dfcd8d7e06a885641af740f83eb95a
Trisha Chittajallu <z5589661@ad.unsw.edu.au>
2025-08-31 23:44:32 +1000

    feat(colours): add colour codes for echo messages

---
+++ colours.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# colours.sh
+
+# colour codes
+PINK='\033[1;35m'
+BLUE='\033[1;36m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;34m'
+RED='\033[1;31m'
+
+RESET='\033[0m'
